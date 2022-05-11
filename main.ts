4705976cf688f279a96ab2be5735076fc853f25b
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-05-11 13:22:50 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+input.onButtonPressed(Button.A, function () {
+    scoremaker = 1
+    radio.sendNumber(scoremaker)
+})
+input.onButtonPressed(Button.AB, function () {
+    scoremaker = 0
+    radio.sendNumber(scoremaker)
+})
+input.onButtonPressed(Button.B, function () {
+    scoremaker = -1
+    radio.sendNumber(scoremaker)
+})
+let scoremaker = 0
+radio.setGroup(0)
+scoremaker = 0
+basic.showNumber(scoremaker)
+basic.forever(function () {
+	
+})
